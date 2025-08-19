8fc18d08c48cc4349fc8ac400fb0118dfe19fdde
Kevin Vieira <xkevet@outlook.com>
2025-08-18 22:14:31 -0300

    :tada: Commit Inicial

---
+++ src/main/java/com/neurotech/desafio/neurotech_desafio/NeurotechDesafioApplication.java
@@ -0,0 +1,13 @@
+package com.neurotech.desafio.neurotech_desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeurotechDesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeurotechDesafioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neurotech/desafio/neurotech_desafio/NeurotechDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neurotech.desafio.neurotech_desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeurotechDesafioApplicationTests {
+
+	//@Test
+	//void contextLoads() {
+	//}
+
+}
