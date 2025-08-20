959c587e9ffe9638511b7b0a21e4c5d559fc6afe
Kevin Vieira <xkevet@outlook.com>
2025-08-19 23:36:38 -0300

    :sparkles: feat: Create produto Repository

---
+++ src/main/java/com/neurotech/desafio/neurotech_desafio/repositories/ProdutoRepository.java
@@ -0,0 +1,10 @@
+package com.neurotech.desafio.neurotech_desafio.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.neurotech.desafio.neurotech_desafio.entities.Produto;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long>{
+
+
+}
