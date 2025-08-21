db7fd51d659b18bef48e7bd34115ec6620944e90
Kevin Vieira <xkevet@outlook.com>
2025-08-20 22:22:34 -0300

    :sparkles: feat: Improve findAll with requests params

--- src/main/java/com/neurotech/desafio/neurotech_desafio/controllers/ProdutoController.java
+++ src/main/java/com/neurotech/desafio/neurotech_desafio/controllers/ProdutoController.java
@@ -1,9 +1,9 @@
 package com.neurotech.desafio.neurotech_desafio.controllers;
 
 import java.net.URI;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -26,11 +27,16 @@ public class ProdutoController{
     @Autowired
     private ProdutoService produtoService;
 
-    @GetMapping
-    public List<ProdutoMinDTO> findAll(){
-
-        return produtoService.findAll();
-
+    
+     @GetMapping
+    public Page<ProdutoMinDTO> findAll(
+        @RequestParam(required = false) String nome,
+        @RequestParam(defaultValue = "0") int page,
+        @RequestParam(defaultValue = "10") int size,
+        @RequestParam(defaultValue = "preco") String sort,
+        @RequestParam(defaultValue = "asc") String direction
+    ) {
+        return produtoService.findAllPaged(nome, page, size, sort, direction);
     }
     
     @GetMapping(value = "/{id}")
