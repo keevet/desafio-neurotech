b66b6cff7022d721307d064570ccb58157035768
Kevin Vieira <xkevet@outlook.com>
2025-08-20 21:47:10 -0300

    :sparkles: feat: add delete method

--- src/main/java/com/neurotech/desafio/neurotech_desafio/controllers/ProdutoController.java
+++ src/main/java/com/neurotech/desafio/neurotech_desafio/controllers/ProdutoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -53,6 +54,12 @@ public class ProdutoController{
     public ResponseEntity<ProdutoDTO> update(@PathVariable Long id, @RequestBody ProdutoDTO dto) {
         ProdutoDTO updatedDto = produtoService.update(id, dto);
         return ResponseEntity.ok(updatedDto);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        produtoService.delete(id);
+        return ResponseEntity.noContent().build(); 
     }
   
 }
