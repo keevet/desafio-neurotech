4b0dcdc013df16a8ad8a1710b32495fb09b7c3e2
Kevin Vieira <xkevet@outlook.com>
2025-08-20 22:19:01 -0300

    :sparkles: feat: Add page interface

--- src/main/java/com/neurotech/desafio/neurotech_desafio/repositories/ProdutoRepository.java
+++ src/main/java/com/neurotech/desafio/neurotech_desafio/repositories/ProdutoRepository.java
@@ -1,10 +1,13 @@
 package com.neurotech.desafio.neurotech_desafio.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.neurotech.desafio.neurotech_desafio.entities.Produto;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long>{
 
+    Page<Produto> findByNomeContainingIgnoreCase(String nome, Pageable pageable);
 
 }
