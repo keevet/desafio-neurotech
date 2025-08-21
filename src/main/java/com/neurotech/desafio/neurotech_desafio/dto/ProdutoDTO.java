7b923eae16d0d1c3ae642f58eda63e7f4d1cda0c
Kevin Vieira <xkevet@outlook.com>
2025-08-20 21:49:48 -0300

    :bug: fix: include empty constructor

--- src/main/java/com/neurotech/desafio/neurotech_desafio/dto/ProdutoDTO.java
+++ src/main/java/com/neurotech/desafio/neurotech_desafio/dto/ProdutoDTO.java
@@ -21,29 +21,59 @@ public class ProdutoDTO {
 
     }
 
+    public ProdutoDTO(){
+        
+    }
+
     public Long getId() {
         return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getNome() {
         return nome;
     }
 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
     public String getDescricao() {
         return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
     }
 
     public Double getPreco() {
         return preco;
     }
 
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
     public Integer getQuantidadeEstoque() {
         return quantidadeEstoque;
+    }
+
+    public void setQuantidadeEstoque(Integer quantidadeEstoque) {
+        this.quantidadeEstoque = quantidadeEstoque;
     }
 
     public LocalDateTime getDataCriacao() {
         return dataCriacao;
     }
+
+    public void setDataCriacao(LocalDateTime dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+   
 
     
 
