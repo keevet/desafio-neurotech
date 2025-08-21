502666710cc416f6966854284b4d9bc5d215cd26
Kevin Vieira <xkevet@outlook.com>
2025-08-20 21:38:44 -0300

    :sparkles: feat: add post method

--- src/main/java/com/neurotech/desafio/neurotech_desafio/controllers/ProdutoController.java
+++ src/main/java/com/neurotech/desafio/neurotech_desafio/controllers/ProdutoController.java
@@ -1,12 +1,19 @@
 package com.neurotech.desafio.neurotech_desafio.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.neurotech.desafio.neurotech_desafio.dto.ProdutoDTO;
 import com.neurotech.desafio.neurotech_desafio.dto.ProdutoMinDTO;
 import com.neurotech.desafio.neurotech_desafio.services.ProdutoService;
 
@@ -24,5 +31,22 @@ public class ProdutoController{
 
     }
     
+    @GetMapping(value = "/{id}")
+    public ProdutoDTO findById(@PathVariable Long id){
 
+        return produtoService.findById(id);
+
+    }
+
+    @PostMapping
+    public ResponseEntity<ProdutoDTO> insert(@RequestBody ProdutoDTO dto) {
+        ProdutoDTO newDto = produtoService.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                        .path("/{id}")
+                        .buildAndExpand(newDto.getId())
+                        .toUri();
+         return ResponseEntity.created(uri).body(newDto);
+    }
+
+  
 }
