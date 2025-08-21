a9c92e6076d7f12a837031b1162001956ce68e88
Kevin Vieira <xkevet@outlook.com>
2025-08-20 23:53:48 -0300

    feat: Add exception resource not found

---
+++ src/main/java/com/neurotech/desafio/neurotech_desafio/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,5 @@
+package com.neurotech.desafio.neurotech_desafio.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String msg) { super(msg); }
+}
