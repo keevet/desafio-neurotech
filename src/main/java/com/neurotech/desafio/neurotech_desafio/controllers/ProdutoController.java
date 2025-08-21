8a124f64a787089e2bda67c598459594919ec2e1
Kevin Vieira <xkevet@outlook.com>
2025-08-20 21:43:30 -0300

    :sparkles: feat: Add put method

--- src/main/java/com/neurotech/desafio/neurotech_desafio/controllers/ProdutoController.java
+++ src/main/java/com/neurotech/desafio/neurotech_desafio/controllers/ProdutoController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,5 +49,10 @@ public class ProdutoController{
          return ResponseEntity.created(uri).body(newDto);
     }
 
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ProdutoDTO> update(@PathVariable Long id, @RequestBody ProdutoDTO dto) {
+        ProdutoDTO updatedDto = produtoService.update(id, dto);
+        return ResponseEntity.ok(updatedDto);
+    }
   
 }
